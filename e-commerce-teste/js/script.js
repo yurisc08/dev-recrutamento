617cfe6ef75f896e185e558abfd6fcfe2fec38ed
yurisc08 <139633237+yurisc08@users.noreply.github.com>
2023-07-17 02:24:06 -0300

    commit inicial

---
+++ e-commerce-teste/js/script.js
@@ -0,0 +1,61 @@
+(function($) {
+
+  "use strict";
+
+  var initPreloader = function() {
+    $(document).ready(function($) {
+    var Body = $('body');
+        Body.addClass('preloader-site');
+    });
+    $(window).load(function() {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    });
+  }
+
+
+  var initProductQty = function(){
+
+    $('.product-qty').each(function(){
+
+      var $el_product = $(this);
+      var quantity = 0;
+
+      $el_product.find('.quantity-right-plus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          $el_product.find('#quantity').val(quantity + 1);
+      });
+
+      $el_product.find('.quantity-left-minus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          if(quantity>0){
+            $el_product.find('#quantity').val(quantity - 1);
+          }
+      });
+
+    });
+
+  }
+
+  var initJarallax = function() {
+    jarallax(document.querySelectorAll(".jarallax"));
+
+    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
+      keepImg: true,
+    });
+  }
+
+
+  $(document).ready(function() {
+    
+    initPreloader();
+    initSwiper();
+    initProductQty();
+    initJarallax();
+    initChocolat();
+
+  }); 
+
+})(jQuery);
